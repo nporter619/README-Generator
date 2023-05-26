12276cc4aab03cdb7204420354cffeebf2e4878f
nporter <nporterwork@gmail.com>
2023-05-26 00:46:12 -0700

    adding to starter code with licences vairbles and sample prompt questions

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
+// licenses to choose from
+const licenses = ["MIT", "GPLv3", "Apache", "Unlicense"];
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project',
+      choices: licenses,
+    },
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    //...
+  }
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    //...
+  }
 // Function call to initialize app
 init();
